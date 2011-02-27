cb650a418f64f915fcf807b0f0a4de1b9e27ad45
David McCann <david.a.mccann@gmail.com>
2011-02-27 14:45:29 +0300

    package restructure, added setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name='healthmodels',
+    version='0.1',
+    license="BSD",
+
+    requires = ["rapidsms (>=0.9.6a)"],
+
+    description='A common set of models for mHealth-related applications.',
+    long_description=open('README.rst').read(),
+    author='David McCann',
+    author_email='david.a.mccann@gmail.com',
+
+    url='http://github.com/daveycrockett/healthmodels',
+    download_url='http://github.com/daveycrockett/healthmodels/downloads',
+
+    include_package_data=True,
+
+    packages=['healthmodels'],
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
