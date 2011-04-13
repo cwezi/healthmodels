f88576f98818fccc3c634dc800db7127c078fec1
Mugisha Moses <moses@appfrica.org>
2011-04-13 12:19:51 +0300

    simple_locations dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     license="BSD",
 
-    install_requires = ["rapidsms"],
+    install_requires = ["rapidsms","simple_locations"],
 
     dependency_links = [
         "http://github.com/mossplix/simple_locations/tarball/master#egg=simple_locations"
